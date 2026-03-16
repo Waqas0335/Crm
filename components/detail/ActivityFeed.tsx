59d792b3e5341ff84ff7189c0e697440293de52a
Waqas Amjad <waqasts335@gmail.com>
2026-03-17 01:46:14 +0500

    new and new changes

--- components/detail/ActivityFeed.tsx
+++ components/detail/ActivityFeed.tsx
@@ -20,7 +20,8 @@ function FeedIcon({ type }: { type: ActivityType }) {
     esign: (<svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"><path d="M12 20h9M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" /></svg>),
     note: (<svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"><path d="M12 20h9M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" /></svg>),
   };
-  return icons[type];
+  return icons[type];  
+  
 }
 
 export default function ActivityFeed() {
